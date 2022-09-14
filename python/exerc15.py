78f499ff87bec84104e9c0aa5b64fb1a89de1cb2
Vitor Odorico <100388613+VitorOdorico@users.noreply.github.com>
2022-09-14 19:54:37 -0300

    Create exerc15.py

---
+++ python/exerc15.py
@@ -0,0 +1,8 @@
+""""Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps), calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos)."""
+
+tamanhoMB = input("Qual o tamanho do arquivo em MB:")
+velocidadeInternetMbps = input("Qual a velocidade da internet em Mbps")
+seg = ()
+calc = tamanhoMB/velocidadeInternetMbps*seg
+
+print("o tempo demorado e de:" + calc)
